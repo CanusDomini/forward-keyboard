da88d756d13a296a43f84ab9e76761a2ce581c54
Your Name <you@example.com>
2017-07-13 06:26:23 +0100

    Init a new forward keyboard project

---
+++ forward_keyboard.rb
@@ -0,0 +1,14 @@
+require 'hidapi'
+
+@osx_hid_info = `system_profiler -xml SPUSBDataType`
+
+
+# Parse the @osx_hid_info variable which is a glob of XML
+# pull out the vendor_id and product_id strings and save them to variables
+
+vendor_id = "" # Get this by parsing the above XML
+product_id = "" # Get that from above xml glob
+
+device = HIDAPI::Open(vendor_id, product_id)
+
+
