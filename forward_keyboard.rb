69e4d7e4babbdcb419c9d29f8a3792e788737d76
x <x@x>
2017-07-13 23:43:22 +0200

    updated to give product/vendor id info fetching functionality

--- forward_keyboard.rb
+++ forward_keyboard.rb
@@ -15,8 +15,8 @@ p @osx_hid_info.class
 # Parse the @osx_hid_info variable which is a glob of XML
 # pull out the vendor_id and product_id strings and save them to variables
 
-vendor_id = "" # Get this by parsing the above XML
-product_id = "" # Get that from above xml glob
+@vendor_id = "" # Get this by parsing the above XML
+@product_id = "" # Get that from above xml glob
 
 #device = HIDAPI::Open(vendor_id, product_id)
 
@@ -29,8 +29,25 @@ hash = Hash.from_xml(@osx_hid_info)
 
 #yaml = hash.to_yaml
 
-#File.open("system_profiler.yaml", "w") do |file|
-#  file.write(yaml)
-#end
+#
+## Map and Reduce
+
+#p hash
+
+# The data we are working on is 3 arrays being used to create the same functionality as a hash.
+p top_level_array = hash['plist']['array']['dict']['array'][1]['dict']['array']['dict'][0]['key']
+p top_level_array_value = hash['plist']['array']['dict']['array'][1]['dict']['array']['dict'][1]['string']
+File.open("system_profiler.debug.yaml", "w") do |file|
+  file.write(top_level_array.to_yaml)
+end
+File.open("system_profiler.debug.yaml", "w") do |file|
+  file.write(top_level_array_value.to_yaml)
+end
+product_id = top_level_array_value[-2]
+vendor_id = top_level_array_value[-1]
+p "The product id: #{product_id}"
+p "The vendor id: #{vendor_id}"
 #
 
+
+@new_hash = {product_id: @product_id, vendor_id: @vendor_id}
